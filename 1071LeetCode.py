509d309798f72d2bd52d70cfa53d98be22e20034
douglasC <douglasc@douglascerrato2.digital>
2024-12-18 23:14:08 -0800

    Fixed what return result should be for test 2

--- 1071LeetCode.py
+++ 1071LeetCode.py
@@ -6,5 +6,5 @@ def gcdOfStrings(str1: str, str2: str) -> str:
     return ""
     
 print(f"Test 1: {gcdOfStrings("ABCABC","ABC") == "ABC"}")
-print(f"Test 2: {gcdOfStrings("ABABAB","ABAB") == "ABAB"}")
+print(f"Test 2: {gcdOfStrings("ABABAB","ABAB") == "AB"}")
 print(f"Test 3: {gcdOfStrings("LEET","CODE") == ""}")
