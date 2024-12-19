97cd44a83fc6d6449691dc4cd8dac81854a11b2c
douglasC <douglasc@douglascerrato2.digital>
2024-12-18 22:49:45 -0800

    Working on LeetCode 1071

---
+++ 1071LeetCode.py
@@ -0,0 +1,14 @@
+# URL
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+
+
+def gcdOfStrings(str1: str, str2: str) -> str:
+    i = 0
+    j = 0    
+    
+    return ""
+    
+    
+print(f"Test 1: {gcdOfStrings("ABCABC","ABC") == "ABC"}")
+print(f"Test 2: {gcdOfStrings("ABABAB","ABAB") == "ABAB"}")
+print(f"Test 3: {gcdOfStrings("LEET","CODE") == ""}")
