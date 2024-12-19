d9a1c84d7e1018042e96f7c5813062d703af03ff
douglasC <douglasc@douglascerrato2.digital>
2024-12-19 07:15:06 -0800

    Completed 283LeetCode.py and Created Completed Folder

---
+++ Completed/283LeetCode.py
@@ -0,0 +1,18 @@
+# URL
+# https://leetcode.com/problems/move-zeroes/description/
+
+def moveZeroes(nums: list[int]) -> list:
+    zeroCount = []
+    x = 0
+    while x != len(nums) - 1:
+        if nums[x] == 0:
+            zeroCount.append(0)
+            nums.pop(x)
+        else:
+            x+=1
+            
+    nums = nums + zeroCount
+    return nums
+
+print(f"Test 1: {moveZeroes([0,1,0,3,12]) == [1,3,12,0,0]}")
+print(f"Test 2: {moveZeroes([0]) == [0]}")
