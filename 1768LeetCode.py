356efaef4687d7e53f4c58f523236965e28fc8b9
douglasC <douglasc@douglascerrato2.digital>
2024-12-18 22:40:06 -0800

    Mistake. Pushing Completed Code

--- 1768LeetCode.py
+++ 1768LeetCode.py
@@ -2,39 +2,18 @@
 # https://leetcode.com/problems/merge-strings-alternately
 
 def mergeAlternately(word1: str, word2: str) -> str:
-#     newString = ""
-#     if(len(word1) > len(word2)): x = len(word1)
-#     else: x = len(word2)
-        
-#     for y in range(x):
-#         if(word1):
-#             newString+=word1[0]
-#             word1 = word1[1:]
-#         if(word2):
-#             newString+=word2[0]
-#             word2 = word2[1:]
-
-#     return newString
     newString = ""
-    for x in range(len(word1)):
-        newString+=word1[:1]
-        if len(word1) > 1:
-            word1=word1[1:]
-        else:
-            word1 = ""
-        print(f"Word1 after slicing last first letter is {word1}")
-        newString+=word2[:1]
+    if(len(word1) > len(word2)): x = len(word1)
+    else: x = len(word2)
         
-        if len(word2) > 1:
-            word2=word2[1:]
-        else:
-            word2 = ""
-        print(f"Word2 after slicing last first letter is {word2}")
-        print(newString)
-    if(len(word1) > len(word2)):
-        newString+=word1
-    else:
-        newString+=word2
+    for y in range(x):
+        if(word1):
+            newString+=word1[0]
+            word1 = word1[1:]
+        if(word2):
+            newString+=word2[0]
+            word2 = word2[1:]
+
     return newString
 
 print(f"Test 1: {mergeAlternately("abc", "pqr")=="apbqcr"}")
