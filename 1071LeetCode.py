31eb1c2baa6ea016fea547954ed59d414ecf4149
douglasC <douglasc@douglascerrato2.digital>
2024-12-20 04:17:40 -0800

    Working on 1071LeetCode
    Made this code so far. However major issue, the test cases on leetcode has examples where str2 is longer then str1. RIP

--- 1071LeetCode.py
+++ 1071LeetCode.py
@@ -3,7 +3,31 @@
 
 
 def gcdOfStrings(str1: str, str2: str) -> str:
-    return ""
+    lenStr1, lenStr2 = len(str1), len(str2)
+    if lenStr1 % lenStr2 != 0:
+        if lenStr1 % (int(lenStr2/2)) != 0:
+            return ""
+        else:
+            j = 0
+            for i in range(lenStr1):
+                if(str1[i] != str2[j]):
+                    return ""
+                if(j == int((lenStr2/2) - 1)):
+                    j = 0
+                else:
+                    j+=1                
+            return str2[:int(lenStr2/2)]
+    else:
+        j = 0
+        for i in range(lenStr1):
+            if(str1[i] != str2[j]):
+                return ""
+            if(j == lenStr2 - 1):
+                j = 0
+            else:
+                j+=1
+
+        return str2                         
     
 print(f"Test 1: {gcdOfStrings("ABCABC","ABC") == "ABC"}")
 print(f"Test 2: {gcdOfStrings("ABABAB","ABAB") == "AB"}")
