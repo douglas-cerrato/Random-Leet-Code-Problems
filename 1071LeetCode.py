b04e58730eada9b91b73cdd1018f2958c50f1b38
douglasC <douglasc@douglascerrato2.digital>
2024-12-21 22:26:17 -0800

    Reworking on 1071 to pass all tests

--- 1071LeetCode.py
+++ 1071LeetCode.py
@@ -3,31 +3,9 @@
 
 
 def gcdOfStrings(str1: str, str2: str) -> str:
-    lenStr1, lenStr2 = len(str1), len(str2)
-    if lenStr1 % lenStr2 != 0:
-        if lenStr1 % (int(lenStr2/2)) != 0:
-            return ""
-        else:
-            j = 0
-            for i in range(lenStr1):
-                if(str1[i] != str2[j]):
-                    return ""
-                if(j == int((lenStr2/2) - 1)):
-                    j = 0
-                else:
-                    j+=1                
-            return str2[:int(lenStr2/2)]
-    else:
-        j = 0
-        for i in range(lenStr1):
-            if(str1[i] != str2[j]):
-                return ""
-            if(j == lenStr2 - 1):
-                j = 0
-            else:
-                j+=1
-
-        return str2                         
+    #TODO: To Complete this solution properly and pass all 
+    # tests, I need to find the gcd of string 1
+    return ""
     
 print(f"Test 1: {gcdOfStrings("ABCABC","ABC") == "ABC"}")
 print(f"Test 2: {gcdOfStrings("ABABAB","ABAB") == "AB"}")
