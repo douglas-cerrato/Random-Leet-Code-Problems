ff07741b8175f50ad2b01b127b2e2c5a1ef8b2c8
douglasC <douglasc@douglascerrato2.digital>
2024-12-29 22:21:49 -0800

    Different version of 392LeetCode.py

--- 392LeetCode.py
+++ 392LeetCode.py
@@ -2,22 +2,32 @@
 # https://leetcode.com/problems/is-subsequence/description/
 
 def isSubsequence(s: str, t: str) -> bool:
-    if(s == ""):
-        return True
-    if(t == ""):
-        return False
-    startingPoint = 0
-    for value in range(len(s)):
-        for secondValue in range(startingPoint ,len(t)):
-            if s[value] == t[secondValue]:
-                print(f"Found a match. {s[value]} == {t[secondValue]}")
-                startingPoint = secondValue + 1
-                if(value == len(s)-1):
+    pointerOne = 0
+    pointerTwo = 0
+    
+    while pointerOne < len(s):
+        for x in range(pointerTwo, len(t)):
+            print(f"x is {x}")
+            if(s[pointerOne] == t[pointerTwo]):
+                print(f"Found a match. pointerOne is {pointerOne} and pointerTwo is {pointerTwo}")
+                if(pointerOne == len(s) - 1):
+                    print("Found a match for the last char in string s")
                     return True
-                else:
-                    break
-            if(secondValue == len(t)-1):
+                
+                # Found a match, this means we move pointerOne to find a match for the next character in 
+                # string s. We also move the pointer in string t to iterate one by one in that string
+                # to look for a match    
+                pointerOne+=1
+                pointerTwo+=1
+                break
+            print(f"pointerTwo is {pointerTwo} and len(t)-1 is {len(t)-1}")
+            if(pointerTwo == len(t)-1):
+                # We have hit the end of string t without finding a match 
                 return False
-
+            
+            # Iterating through the next position in pointer Two
+            pointerTwo+=1
+            
+            
 print(f"Test 1: {isSubsequence("abc", "ahbgdc") == True}")
-print(f"Test 2: {isSubsequence("axc", "ahbgdc") == True}")
+print(f"Test 2: {isSubsequence("axc", "ahbgdc") == False}")
